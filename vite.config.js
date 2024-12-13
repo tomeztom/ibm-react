68810877be656d218fe73ff31711d9d2e793195e
Tom Entholzner <tom.entholzner@gmail.com>
2024-12-13 11:14:13 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "ibm-react/dist",
-  plugins: [react()],
+    base: "./",
+    plugins: [react()],
 })
